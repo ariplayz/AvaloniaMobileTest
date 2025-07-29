1afb61c1592506dc79492b33d8e38e6ef6caed78
Ari Cummings <ari@aricummings.com>
2025-07-28 22:54:51 -0400

    Initial commit

---
+++ AvaloniaMobileTest/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaMobileTest.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
